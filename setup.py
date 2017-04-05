4b7a8f91e0326d90f0e42c3ccdb47dc033e48bf3
Danielle Madeley <danielle.madeley@squareweave.com.au>
2017-04-05 11:36:33 +1000

    Add Django 1.11 to Tox

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Framework :: Django',
         'License :: OSI Approved :: BSD License',
     ],
