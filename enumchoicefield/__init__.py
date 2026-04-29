e81dfd14a29562adf05b5e24dbaecc4f9b7d51d3
Tim Heap <tim.heap@csiro.au>
2026-04-29 15:58:35 +1000

    Use pyproject.toml for project metadata

--- enumchoicefield/__init__.py
+++ enumchoicefield/__init__.py
@@ -1,5 +1,8 @@
+import importlib.metadata
+
 from .enum import ChoiceEnum
 from .fields import EnumChoiceField
-from .version import version
 
 __all__ = ['ChoiceEnum', 'EnumChoiceField', 'version']
+
+version = importlib.metadata.version('django-enumchoicefield')
